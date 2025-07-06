d40b1d60f7954570283c5d0ab575ba0f187a58c9
Wassim Akkari <was.akkari@gmail.com>
2025-07-06 22:28:41 +0100

    Add some features.

--- src/root.zig
+++ src/root.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn parse(T: type, src: []const u8) !T {
     _ = src;
-    std.debug.print("TODO: parse json!\n", .{});
+    std.debug.print("TODO: parse json and add more features\n", .{});
     return undefined; // dont't do this for demonstration purpses
 }
 
