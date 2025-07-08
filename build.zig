61eaead2dceb56675461a790f13beedb95eb0363
Wassim Akkari <was.akkari@gmail.com>
2025-07-08 18:42:07 +0100

    Fix comments.

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) !void {
 
     const mod = b.addModule("json", .{ .root_source_file = b.path("src/root.zig") });
 
-    const exe = b.addExecutable(.{
+    const exe = b.addExecutable(.{                                                      // doesn't expose it to the package consumer
         .name = "json",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
--- src/root.zig
+++ src/root.zig
@@ -3,6 +3,6 @@ const std = @import("std");
 pub fn parse(T: type, src: []const u8) !T {
     _ = src;
     std.debug.print("TODO: parse json and add more features\n", .{});
-    return undefined; // dont't do this for demonstration purpses
+    return undefined; // don't do this for demonstration purpses
 }
 
